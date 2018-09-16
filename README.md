e1e3857f2678209bef311cafbab14d2a0eb866d8
ddrivero <43010909+ddrivero@users.noreply.github.com>
2018-09-16 15:24:51 -0400

    Copied first repo
    Copied all other repo files in to this one

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ece309-fall18-ddrivero
+# ece309-fall18-unityid
+Repository for students to fork and modify.
+Dominic Rivero
+ECE 309
+9/5/18
+
+Change List:  
+* HW2: forked the repo, added collaborators to repo, added change list to README.md  
+* Future updates
---
+++ lib/HW2.cpp
@@ -0,0 +1,108 @@
+#include <string>
+
+using namespace std;
+
+class ListNode{
+  private:
+    string word;
+    ListNode *next;
+   public:
+    ListNode(string a)
+      {word = a; next = NULL; }
+    ListNode* getNext() { return next; }
+    void setNext(ListNode *n) { next = n; }
+    string getWord() { return word; }
+};
+
+Class List{
+  private:
+    ListNode *head;
+    ListNode *tail;
+  public:
+      List(){
+          head = NULL;
+          tail = NULL;      
+      }
+      ~List()
+      {
+        string t;
+        while(!empty())
+        {
+          remove(t);
+        }
+      }
+      
+      void append(string a)
+      {
+        ListNode *node = new ListNode(a);
+        if (head == NULL)
+          {
+            head = node;
+            tail = node;
+          } else {
+            tail->setNExt(node);
+            tail = node;
+          }
+      }
+      
+      bool empty()
+      {
+        return head == NULL;
+      }
+      
+      void push_back(string a)
+      {
+        append(a);
+      }
+      
+      ListNode get(int n)
+      {
+        ListNode *a = head;
+        for(int i = 1; i < n; i++)
+        {
+          a = a->getNext();
+        }
+        return a;
+      }
+      
+      int length()
+      {
+        ListNode *a = head;
+        int n = 0;
+        while(a != NULL)
+        {
+          n++;
+          a = a->getNext();
+        }        
+      }
+  
+      bool remove(string &copy)
+      {
+        if (!empty())
+        {
+          copy = head->getWord();
+          ListNode *tmp = head->getNext();
+          delete head;
+          head = temp;
+          if (temp == NULL)
+            tail = NULL;
+          return true;
+        }
+        return false;
+      }
+      
+      string remove_front()
+      {
+        string a;
+        remove(a);
+        return a;
+      }
+      
+      
+};
+
+int main(){
+  //nothing here
+
+  return 0;
+}
